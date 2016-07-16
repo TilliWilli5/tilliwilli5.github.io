0337b8408847d182b16d43bc9317116b4e9cf16c
TilliWilli_Home <tilliwilli@gmail.com>
2016-07-16 19:34:02 +0300

    Пано крутиться автоматически. При наведении мышки. Пришлось отказаться потому что иначе надо допиливать автоматическую смену всплывающих фраз. Потом как нибудь может доделаю

--- bg_mover.js
+++ bg_mover.js
@@ -1,22 +1,26 @@
+var isActiveMouseBGMover = true;
 $(document).ready(function(){
     var sensivity = -0.1;
     var theHeadBG = $("#header").children()[0];
     document.onmousemove = function(pEvent){
-        var oldPosition = theHeadBG.style.backgroundPosition;
-        var oldX, oldY;
-        var newX, newY;
-        if(oldPosition === "")
+        if(isActiveMouseBGMover)
         {
-            oldX = pEvent.clientX;
-            oldY = pEvent.clientY;
+            var oldPosition = theHeadBG.style.backgroundPosition;
+            var oldX, oldY;
+            var newX, newY;
+            if(oldPosition === "")
+            {
+                oldX = pEvent.clientX;
+                oldY = pEvent.clientY;
+            }
+            else
+            {
+                oldX = parseFloat(oldPosition.split(" ")[0].replace("px",""));
+                oldY = parseFloat(oldPosition.split(" ")[1].replace("px",""));
+            }
+            newX = oldX + pEvent.movementX*sensivity;
+            newY = oldY + pEvent.movementY*sensivity;
+            theHeadBG.style.backgroundPosition = `${newX}px ${newY}px`;
         }
-        else
-        {
-            oldX = parseFloat(oldPosition.split(" ")[0].replace("px",""));
-            oldY = parseFloat(oldPosition.split(" ")[1].replace("px",""));
-        }
-        newX = oldX + pEvent.movementX*sensivity;
-        newY = oldY + pEvent.movementY*sensivity;
-        theHeadBG.style.backgroundPosition = `${newX}px ${newY}px`;
     };
 });
--- principlesSequenceGenerator.js
+++ principlesSequenceGenerator.js
@@ -13,11 +13,39 @@ var principles = [
     ];
 $(document).ready(function(){
     var currentPrinciples = Array.from(principles);
+    var timer;
     $("#header h1")[0].onmouseenter = function(pEvent){
         if(currentPrinciples.length === 0)
                 currentPrinciples = Array.from(principles);
         var currIndex = 0;
         pEvent.target.innerHTML = currentPrinciples[currIndex];
         currentPrinciples.splice(currIndex, 1);
+        isActiveMouseBGMover = false;
+        timer = setInterval(MoveBG, 40, 1, 0.0);
+    };
+    $("#header h1")[0].onmouseleave = function(pEvent){
+        isActiveMouseBGMover = true;
+        clearInterval(timer);
+    }
+    //Auto move BG when mouseover
+    var theHeadBG = $("#header").children()[0];
+    function MoveBG(pDeltaX, pDeltaY){
+        var oldPosition = theHeadBG.style.backgroundPosition;
+        var oldX, oldY;
+        var newX, newY;
+        if(oldPosition === "")
+        {
+            oldX = 0;
+            oldY = 0;
+        }
+        else
+        {
+            oldX = parseFloat(oldPosition.split(" ")[0].replace("px",""));
+            oldY = parseFloat(oldPosition.split(" ")[1].replace("px",""));
+        }
+
+        newX = oldX + pDeltaX;
+        newY = oldY + pDeltaY;
+        theHeadBG.style.backgroundPosition = `${newX}px ${newY}px`;
     }
 });
